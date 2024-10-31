b65c3a19f771c4445d83aa6c48c44a7a56bf23f7
adrian ps <147395349+adrianpereirasantana@users.noreply.github.com>
2024-10-31 11:59:13 -0300

    .

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("BRASIL")
+ function criarcartao(pergunta,resposta){
+      let conteudo = document.getElementById("conteudo")
+      let cartao = document.createElement('article')
+
+
+
+ }
+  criarcartao(1,2)
