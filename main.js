9b38806c9fe3c58b418a3e9f88967fd391e4bbd7
adrian ps <147395349+adrianpereirasantana@users.noreply.github.com>
2024-10-31 12:05:26 -0300

    aa

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ alert("BRASIL")
  function criarcartao(pergunta,resposta){
       let conteudo = document.getElementById("conteudo")
       let cartao = document.createElement('article')
+      cartao.classList = "cartao"
 
 
+    cartao.innerHTML = `
+     <div class="cartao--conteudo">
+                <div class="cartao--pergunta"</p> em que país aconteceu a copa de 2002?</p></div>
+                <div class="cartao--resposta"</p> japão e coreia do sul</p></div>
+                </div>
+    `
+
+      conteudo.appendChild(cartao)
 
  }
   criarcartao(1,2)
